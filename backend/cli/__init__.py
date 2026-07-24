4339a03517b2d578828e53406a0fbbeed88d54bc
tule <tuleaibk@gmail.com>
2026-07-24 09:05:47 +0700

    feat(chunk): quick demo chunking
    Implement simple chunking logic

--- backend/cli/__init__.py
+++ backend/cli/__init__.py
@@ -1,12 +1,13 @@
 import typer
 
 from .parse_code import cli as parse_code_command
+from .chunking import cli as chunk_command
 
 cli = typer.Typer()
 
 
-cli.add_typer(parse_code_command, name="parse", help="Parse source code to tree.")
-
+cli.add_typer(parse_code_command)
+cli.add_typer(chunk_command)
 
 def run() -> None:
     cli()
