bdd08be07efcd6f992376d6d59cb05f839609a4b
Tai0colaocacho <huynhphtai@gmail.com>
2026-07-25 14:31:44 +0700

    refactor: stabilize javascript parser semantics

--- backend/app/parser/languages/javascript/query.scm
+++ backend/app/parser/languages/javascript/query.scm
@@ -1,33 +1,119 @@
-; JavaScript declarations
+; JavaScript and JSX -> UAST query
+;
+; Query captures identify semantic boundaries.
+; Field extraction is performed by JavaScript-specific handlers.
 
-(function_declaration
-  name: (identifier) @meta.name) @definition.function
+; ---------------------------------------------------------------------------
+; Documentation
+; ---------------------------------------------------------------------------
 
-(class_declaration
-  name: (identifier) @meta.name) @definition.class
-
-(method_definition
-  name: (property_identifier) @meta.name) @definition.method
-
-(variable_declarator
-  name: (identifier) @meta.name) @definition.variable
-
-
-; JavaScript imports
-
-(import_statement
-  source: (string) @meta.module_path) @dependency.import
+(
+  (comment) @meta.doc
+  (#match? @meta.doc "^/\\*\\*")
+)
 
 
-; Direct function calls: execute()
+; ---------------------------------------------------------------------------
+; Type definitions
+; ---------------------------------------------------------------------------
 
-(call_expression
-  function: (identifier) @meta.name) @reference.call
+(class_declaration) @definition.class
 
 
-; Member calls: service.execute()
+; ---------------------------------------------------------------------------
+; Constructors and methods
+; ---------------------------------------------------------------------------
 
-(call_expression
-  function: (member_expression
-    object: (identifier) @meta.subject
-    property: (property_identifier) @meta.name)) @reference.call
+(
+  (method_definition
+    name: (property_identifier) @_.constructor_name)
+    @definition.constructor
+  (#eq? @_.constructor_name "constructor")
+)
+
+(method_definition) @definition.method
+
+
+
+; ---------------------------------------------------------------------------
+; Functions
+; ---------------------------------------------------------------------------
+
+(function_declaration) @definition.function
+
+(generator_function_declaration) @definition.function
+
+
+
+; ---------------------------------------------------------------------------
+; Parameters
+; ---------------------------------------------------------------------------
+
+(formal_parameters
+  (identifier) @definition.parameter)
+
+(formal_parameters
+  (assignment_pattern) @definition.parameter)
+
+(formal_parameters
+  (rest_pattern) @definition.parameter)
+
+(formal_parameters
+  (object_pattern) @definition.parameter)
+
+(formal_parameters
+  (array_pattern) @definition.parameter)
+
+
+; Arrow function without parentheses: value => value + 1
+
+(arrow_function
+  parameter: (identifier) @definition.parameter)
+
+
+; ---------------------------------------------------------------------------
+; Variables, constants and fields
+; ---------------------------------------------------------------------------
+
+
+(variable_declarator) @definition.variable
+
+(field_definition) @definition.field
+
+
+; ---------------------------------------------------------------------------
+; Dependencies
+; ---------------------------------------------------------------------------
+
+(import_statement) @dependency.import
+
+
+; export { value } from "./module.js"
+
+(export_statement
+  source: (string)) @dependency.import
+
+
+; const package = require("package")
+
+(
+  (call_expression
+    function: (identifier) @_.require_function
+    arguments: (arguments
+      (string) @_.require_source))
+    @dependency.import
+  (#eq? @_.require_function "require")
+)
+
+
+; ---------------------------------------------------------------------------
+; References
+; ---------------------------------------------------------------------------
+
+(call_expression) @reference.call
+
+(new_expression) @reference.call
+
+(member_expression) @reference.attribute
+
+(subscript_expression) @reference.attribute
